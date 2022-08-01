15d85beb5c7a259592c4b630d3cae9cab001d93e
Unnamed-1408 <1210552276@qq.com>
2022-08-01 19:14:23 +0800

    edit 2d

--- Deep_Learning_Based_DNA_Sequence_Fast_Decoding/models.py
+++ Deep_Learning_Based_DNA_Sequence_Fast_Decoding/models.py
@@ -25,7 +25,7 @@ def unet_model(max_len, vocab_size):
     inputs = Input((max_len, vocab_size))
     conv1 = Conv1D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
     conv1 = Conv1D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
-    pool1 = MaxPooling1D(pool_size=(2, 2))(conv1)
+    pool1 = MaxPooling1D(pool_size=4)(conv1)
     conv2 = Conv1D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
     conv2 = Conv1D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
     pool2 = MaxPooling1D(pool_size=4)(conv2)
@@ -43,7 +43,7 @@ def unet_model(max_len, vocab_size):
 
     up6 = Conv1D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
         UpSampling1D(size=4)(drop5))
-    merge6 = concatenate([drop4, up6], axis=3)
+    merge6 = concatenate([drop4, up6], axis=2)
     conv6 = Conv1D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
     conv6 = Conv1D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
 
@@ -55,19 +55,19 @@ def unet_model(max_len, vocab_size):
 
     up8 = Conv1D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
         UpSampling1D(size=4)(conv7))
-    merge8 = concatenate([conv2, up8], axis=3)
+    merge8 = concatenate([conv2, up8], axis=2)
     conv8 = Conv1D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
     conv8 = Conv1D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
 
     up9 = Conv1D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
         UpSampling1D(size=4)(conv8))
-    merge9 = concatenate([conv1, up9], axis=3)
+    merge9 = concatenate([conv1, up9], axis=2)
     conv9 = Conv1D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
     conv9 = Conv1D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
     conv9 = Conv1D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
     conv10 = Conv1D(1, 1, activation='sigmoid')(conv9)
 
-    model = Model(input=inputs, output=conv10)
+    model = Model(inputs=inputs, outputs=conv10)
 
     # using args to config
     # model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])

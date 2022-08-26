1709d11b3539aa372321554dc77cd56457c2cd93
Unnamed-1408 <1210552276@qq.com>
2022-08-26 18:16:17 +0800

    test

--- Deep_Learning_Based_DNA_Sequence_Fast_Decoding/models.py
+++ Deep_Learning_Based_DNA_Sequence_Fast_Decoding/models.py
@@ -64,8 +64,8 @@ def unet_model(max_len, vocab_size):
     merge9 = concatenate([conv1, up9], axis=2)
     conv9 = Conv1D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
     conv9 = Conv1D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-    conv9 = Conv1D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-    conv10 = Conv1D(5, 1, activation='sigmoid')(conv9)
+    conv9 = Conv1D(vocab_size, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+    conv10 = Conv1D(1, 1, activation='sigmoid')(conv9)
 
     model = Model(inputs=inputs, outputs=conv10)
 
